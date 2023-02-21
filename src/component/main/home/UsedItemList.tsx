15784f64edb73748414cd2813bc951c3163d7659
kyoohyun <kgh2120@naver.com>
2023-02-21 16:02:15 +0900

    GM-305 중고 거래 아이템 조회 페이지 리프래시

--- src/component/main/home/UsedItemList.tsx
+++ src/component/main/home/UsedItemList.tsx
@@ -1,13 +1,13 @@
 import styled from "styled-components";
 import UsedItem from "./UsedItem";
-import {createDummyUsedItemData} from "../../../util/Dummy";
 import {usedItemApi} from "../../../api/usedItemApi";
-import {useEffect, useRef, useState} from "react";
-import {currentTime} from "../../../util/CurrentTime";
+import {useEffect} from "react";
 import {useInView} from "react-intersection-observer";
 import {ADD, useUsedItemSelector} from "../../../store/usedItem";
 import {useDispatch} from "react-redux";
 import {PLUS, usePageNumSelector} from "../../../store/pageNum";
+import {UsedItemPost} from "../../../model/usedItemPost";
+import {useNowSelector} from "../../../store/now";
 
 const UsedItemListWrapper = styled.div`
   margin: 20px;
@@ -18,20 +18,20 @@ function UsedItemList() {
 
 
     const [ref, inView] = useInView();
-    const usedItem = useUsedItemSelector();
+    const usedItem:UsedItemPost[] = useUsedItemSelector();
     const pageNum = usePageNumSelector();
     const dispatch = useDispatch();
 
 
-    const [now,setNow]=useState<string>(currentTime());
+    const now = useNowSelector();
     let query = usedItemApi.useGetUsedItemQuery({
         pageNum,
         requestTime : now
     });
     useEffect(() => {
 
-        if (inView && !query.isLoading && query.data !== null) {
-            console.log(pageNum)
+        if (inView && !query.isLoading && query.data !== null && usedItem.length > 0) {
+            console.log(`hooking~~ ${pageNum}`)
             dispatch(PLUS())
         }
     }, [inView])
@@ -43,7 +43,7 @@ function UsedItemList() {
         if (query.isSuccess) {
             dispatch(ADD(query.data));
         }
-    },[query.data ,pageNum])
+    },[query.data,now])
 
     if(query.isLoading) return <>Now Loading...</>
 
